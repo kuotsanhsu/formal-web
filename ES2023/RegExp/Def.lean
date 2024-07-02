9ca6c0d9b882fe7859f2bdeef0a2b6a2be783555
kuotsanhsu <learningstud@gmail.com>
2024-07-02 23:24:57 +0800

    RegExp: good

--- ES2023/RegExp/Def.lean
+++ ES2023/RegExp/Def.lean
@@ -1,63 +1,42 @@
-import ES2023.RegExp.Prelude
-
 /-- https://en.wikipedia.org/wiki/Regular_language#Formal_definition
-`α` is the type of alphabet allowed in the regular language. We make it  universe-polymorphic. -/
+`α` is the type of alphabet allowed in the regular language. We make it universe-polymorphic. -/
 inductive RegExp.{u} (α: Type u)
   /-- Matches nothing. -/
-  | protected nothing
+  | nothing
   /-- Empty string matcher. -/
-  | ε
-  | protected single (a: α)
-  | protected append (r₁ r₂: RegExp α) -- "ab"
-  | protected union (r₁ r₂: RegExp α) -- "x(a|b)y"
-  -- append x (append (union a b) y)
-  | protected star (r: RegExp α) -- "x*": ""
-
-export RegExp (ε)
+  | empty
+  | single (a: α)
+  /-- `/ab/` -/
+  | append (r₁ r₂: RegExp α)
+  /-- `/a|b/`-/
+  | union (r₁ r₂: RegExp α)
+  | star (r: RegExp α)
 
 namespace RegExp
 universe u
-variable {α: Type u}
+variable {α : Type u}
 
 /-!
 Define convenient notations for `RegExp`.
 -/
 
-/-- Write `∅` for `RegExp.empty`. -/
-@[default_instance] instance: EmptyCollection (RegExp α) where
-  emptyCollection := .nothing
+instance : EmptyCollection (RegExp α) where
+  emptyCollection := empty
 
-/-- Write `a: α` for `RegExp.char a`. -/
-instance: Coe α (RegExp α) where
-  coe := .single
-
-/-- In case of `0: RegExp Nat`. -/
-instance {n}: OfNat (RegExp Nat) n where
-  ofNat := n
-#check (0: RegExp Nat)
+instance : Singleton α (RegExp α) where
+  singleton := single
 
 /-- Write `r ++ r'` for `RegExp.append r r'`. -/
-instance: Append (RegExp α) where
-  append := .append
+instance : Append (RegExp α) where
+  append := append
 
 /-- Write `r ∪ r'` for `RegExp.union r r'`. -/
-@[default_instance, match_pattern] instance: Union (RegExp α) where
-  union := .union
+instance : Union (RegExp α) where
+  union := union
 
 /-- Write `r*` for `RegExp.star r`. This postfix operator `*` binds tighter
 than function application. -/
-instance: Star (RegExp α) where
-  star := .star
-
-/-- Constructs a regular expression that matches exactly the string that it
-receives as an argument. -/
-def fromStr: List α → RegExp α
-  | [] => ε
-  | x::xs => x ++ fromStr xs
-
-instance: Coe (List α) (RegExp α) where
-  coe := fromStr
-#check_failure ([]: RegExp α) -- FIXME
+postfix:arg "*" => star
 
 /-- Denoted by the Haskell operator `=~` to avoid overloading `∈` as done in
 theoretical computer science; really, `∈` is way too overloaded in math. We
@@ -65,8 +44,8 @@ depart slightly from standard practice in that we do not require the type `α`
 to be finite. This results in a somewhat different theory of regular
 expressions, but the difference doesn't concern us here. -/
 inductive accept: List α → RegExp α → Prop
-  | empty: accept [] ε
-  | single {a: α}: accept [a] a
+  | empty: accept [] {}
+  | single {a: α}: accept [a] {a}
   | append {s₁ r₁ s₂ r₂}
     : accept s₁ r₁ → accept s₂ r₂ → accept (s₁ ++ s₂) (r₁ ++ r₂)
   | unionL {s₁ r₁ r₂}: accept s₁ r₁ → accept s₁ (r₁ ∪ r₂)
@@ -74,11 +53,6 @@ inductive accept: List α → RegExp α → Prop
   | starEmpty {r}: accept [] r*
   | starAppend {s₁ s₂ r}: accept s₁ r → accept s₂ r* → accept (s₁ ++ s₂) r*
 
-export accept (empty single append unionL unionR starEmpty starAppend)
-
 @[inherit_doc] infix:50 " =~ " => accept
 
 end RegExp
-
--- Lean4, Coq (OCaml), Agda (Haskell)
--- Dependent type
